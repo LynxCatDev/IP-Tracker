03bb2c86d759d7bf923e6432c99395ce1b2b0faf
Theodore <uzuntudor@yahoo.com>
2025-10-20 09:16:33 +0300

    ip details skeleton and animation

--- components/IPDetails/IPDetails.tsx
+++ components/IPDetails/IPDetails.tsx
@@ -6,6 +6,7 @@ import { CopyButton } from '../CopyButton/CopyButton';
 import { useIPDetails } from './useIPDetails';
 import { Button } from '../Button/Button';
 import { DetailsCard, DetailsCardProps } from '../DetailsCard/DetailsCard';
+import { IPDetailsSkeleton } from './IPDetailsSkeleton';
 import { IP_DETAILS_DATA } from '@/constants/ipDetailsData';
 import './IPDetails.scss';
 
@@ -19,14 +20,7 @@ export const IPDetails = ({ ip }: IPDetailsProps) => {
   const ipDetails = useMemo(() => ipData && IP_DETAILS_DATA(ipData), [ipData]);
 
   if (loading) {
-    return (
-      <div className="ip-details">
-        <div className="ip-details--loading">
-          <div className="spinner" />
-          <p>Fetching your IP information...</p>
-        </div>
-      </div>
-    );
+    return <IPDetailsSkeleton />;
   }
 
   if (error) {
