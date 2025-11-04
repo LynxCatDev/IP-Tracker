d4e47567fac5ea233aa3b5de1c4212a68bc8b5d2
Theodore <uzuntudor@yahoo.com>
2025-11-04 18:49:20 +0200

    support page start

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,5 +1,6 @@
 import { Headphones } from 'lucide-react';
-import { PageInfo } from '@/components';
+import { ContactForm, PageInfo, SuggestionsCards } from '@/components';
+import { supportSuggestions } from '@/constants/suggestionsData';
 
 export default function SupportPage() {
   return (
@@ -9,6 +10,13 @@ export default function SupportPage() {
         description="We're here to help! Get the support you need, when you need it."
         icon={<Headphones size={32} />}
       />
+
+      <SuggestionsCards
+        headerTitle="Common Issues & Solutions"
+        suggestionsList={supportSuggestions}
+      />
+
+      <ContactForm style={{ marginTop: '32px' }} />
     </div>
   );
 }
