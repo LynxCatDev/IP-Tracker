3159f5ab593a4c3f1fb2372c4b940b50f7b764ab
Theodore <uzuntudor@yahoo.com>
2025-12-11 18:58:12 +0200

    optimized css, ipv6 better text, better ip page

--- app/ip/[ip]/page.tsx
+++ app/ip/[ip]/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 
-import { IPDetails } from '@/components';
 import { useParams } from 'next/navigation';
+import { BannerAd, DNSLookup, IPAddressLookup, IPDetails } from '@/components';
 
 export default function IPPage() {
   const { ip } = useParams();
   const ipValue = Array.isArray(ip) ? ip[0] : ip;
   return (
     <div className="ip-page">
+      <BannerAd />
       <IPDetails ip={ipValue} />
+      <IPAddressLookup />
+      <DNSLookup />
     </div>
   );
 }
